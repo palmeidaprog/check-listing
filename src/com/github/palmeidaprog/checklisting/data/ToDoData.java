b50bf88a6dd03dd4c4bdd1830a5e15ffb4604b7e
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-01-26 00:30:05 -0300

    [Sync save] Implementing ToDoData class

--- src/com/github/palmeidaprog/checklisting/data/ToDoData.java
+++ src/com/github/palmeidaprog/checklisting/data/ToDoData.java
@@ -7,41 +7,63 @@ package com.github.palmeidaprog.checklisting.data;
 * @email palmeidaprogramming@gmail.com
 */
 
+import com.sun.xml.internal.bind.v2.TODO;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.CheckBox;
+import java.io.Serializable;
 
-public class ToDoData {
-    public CheckBox getCheck() {
-        return check;
-    }
-
+public class ToDoData implements Serializable {
     // data
-    private transient CheckBox check = new CheckBox();
-
+    private transient CheckBox check;
     private String category, description;
+    private boolean checked = false; // stores CheckBox
 
+    // created new todoTable member contructor
+    public ToDoData() {
+        check = new CheckBox();
+        check.setSelected(false);
+        createCheckBoxEvent();
+    }
 
     //--Data Model-------------------------------------------------------
 
-    public void setCheck(CheckBox check) {
-        this.check = check;
+    public CheckBox getCheck() {
+        return check;
     }
 
     public String getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCategory(String cat) {
+        category = cat;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String d) {
+        description = d;
     }
 
+    //--Serializable interface implementation-----------------------------------------------
+
+    public String toString() {
+        return "category=" + category +"; descrition=" + description + "checked=" + checked;
+    }
+
+    //--Other Serialization related and object write/read methods----------------------------
+    private void createCheckBoxEvent() {
+        check.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                checked = check.isSelected();
+                System.out.println();
+            }
+        });
+    }
 
 
 
