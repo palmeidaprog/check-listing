cc4bf808cee06ec04dcaa34839a27130955e3fd5
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-01-29 11:04:56 -0300

    Added OS X menu code

--- src/com/github/palmeidaprog/checklisting/main/Main.java
+++ src/com/github/palmeidaprog/checklisting/main/Main.java
@@ -12,10 +12,15 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -39,9 +44,9 @@ public class Main extends Application {
         mainStage.getIcons().add(new Image(getClass()
                 .getResourceAsStream("../resources/todo_trans128.png")));
 
-        // Mac OS X specific code TODO: Only use when compiling in OS X
-/*        com.apple.eawt.Application.getApplication().setDockIconImage(ImageIO.read(getClass()
-                .getResourceAsStream("../resources/todo_trans128.png")));*/
+        if(System.getProperty("os.name").toLowerCase().contains("mac")) {
+            osx();
+        }
 
         mainStage.setTitle("Check Listing v0.1 alpha - To Do List Manager");
         mainStage.setScene(new Scene(root, WIDTH, HEIGHT));
@@ -54,6 +59,30 @@ public class Main extends Application {
                 MainController.getInstance().updateObjOnClose();
             }
         });
+    }
+
+
+    public void osx() {
+        // Mac OS X specific code TODO: Only use when compiling in OS X
+        try {
+            com.apple.eawt.Application.getApplication().setDockIconImage(ImageIO.read(getClass()
+                    .getResourceAsStream("../resources/todo_trans128.png")));
+        } catch(IOException e) {
+            System.err.println("Couldn't find the icon");
+            e.printStackTrace();
+        }
+
+        /*MenuBar menu = new MenuBar();
+        Menu file = new Menu();
+        MenuItem quit = new MenuItem();
+        quit.setText("Quit");
+        file.setText("File");
+        file.getItems().add(quit);
+        menu.getMenus().add(file);
+        menu.useSystemMenuBarProperty().set(true);*/
+
+        //com.apple.eawt.Application.getApplication().setDefaultMenuBar(menu);
+
     }
 
     public static Stage getStage() {
