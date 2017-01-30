47a961db9b6db9b87b0ac7335fefd1cb31baaecf
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-01-30 09:27:58 -0300

    [Sync save] Solving artifacts problems

--- src/com/github/palmeidaprog/checklisting/main/Main.java
+++ src/com/github/palmeidaprog/checklisting/main/Main.java
@@ -18,7 +18,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.IOException;
@@ -44,9 +43,9 @@ public class Main extends Application {
         mainStage.getIcons().add(new Image(getClass()
                 .getResourceAsStream("../resources/todo_trans128.png")));
 
-        if(System.getProperty("os.name").toLowerCase().contains("mac")) {
+        /*if(System.getProperty("os.name").toLowerCase().contains("mac")) {
             osx();
-        }
+        }*/
 
         mainStage.setTitle("Check Listing v0.1 alpha - To Do List Manager");
         mainStage.setScene(new Scene(root, WIDTH, HEIGHT));
@@ -62,28 +61,28 @@ public class Main extends Application {
     }
 
 
-    public void osx() {
+    /*public void osx() {
         // Mac OS X specific code TODO: Only use when compiling in OS X
-/*        try {
+*//*        try {
             com.apple.eawt.Application.getApplication().setDockIconImage(ImageIO.read(getClass()
                     .getResourceAsStream("../resources/todo_trans128.png")));
         } catch(IOException e) {
             System.err.println("Couldn't find the icon");
             e.printStackTrace();
-        }*/
+        }*//*
 
-        /*MenuBar menu = new MenuBar();
+        *//*MenuBar menu = new MenuBar();
         Menu file = new Menu();
         MenuItem quit = new MenuItem();
         quit.setText("Quit");
         file.setText("File");
         file.getItems().add(quit);
         menu.getMenus().add(file);
-        menu.useSystemMenuBarProperty().set(true);*/
+        menu.useSystemMenuBarProperty().set(true);*//*
 
         //com.apple.eawt.Application.getApplication().setDefaultMenuBar(menu);
 
-    }
+    }*/
 
     public static Stage getStage() {
         return mainStage;
